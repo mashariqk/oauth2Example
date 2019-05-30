797114d771b0ff2368b19a49f320375046607bdb
Mashariq Khan <Mashariq.Khan@eastbay.com>
2019-05-30 10:43:41 -0500

    Added a custom filter to intercept Oauth2 requests before they reach the spring token endpoint

--- src/main/java/com/oauth2/example/config/WebSecurityConfig.java
+++ src/main/java/com/oauth2/example/config/WebSecurityConfig.java
@@ -1,15 +1,18 @@
 package com.oauth2.example.config;
 
+import com.oauth2.example.filters.PreOauth2Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.annotation.Resource;
 
@@ -31,5 +34,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(userPasswordEncoder);
+    }
+
+    /**
+     * Override this method to configure the {@link HttpSecurity}. Typically subclasses
+     * should not invoke this method by calling super as it may override their
+     * configuration. The default configuration is:
+     *
+     * <pre>
+     * http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
+     * </pre>
+     *
+     * @param http the {@link HttpSecurity} to modify
+     * @throws Exception if an error occurs
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterAfter(new PreOauth2Filter(), BasicAuthenticationFilter.class);
     }
 }
